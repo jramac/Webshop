d2ab5d0e987d6216b883d4f0f3a8b1a964c121cc
jramac <jramac7@gmail.com>
2024-01-23 21:24:35 +0100

    dodan picture slider

--- client/eshop/src/pages/Home/Home.jsx
+++ client/eshop/src/pages/Home/Home.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
-
+import Slider from '../../components/slider/Slider';
+import "./Home.scss";
+import FeaturedProducts from '../../components/featuredProducts/FeaturedProducts';
 const Home = () => {
     return (
-        <div>Home</div>
+        <div className='home'>
+            <Slider/>
+            <FeaturedProducts type="featured"/>
+            <FeaturedProducts type="trending"/>
+        </div>
     );
 };
 
